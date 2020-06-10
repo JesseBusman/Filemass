4d39309d7374a8b25bb1e995d0df768dbc9c3c81
Jesse Busman <jesse@jesbus.com>
2020-06-10 16:24:27 +0200

    Blegh

--- Filemass/path_pattern.cpp
+++ Filemass/path_pattern.cpp
@@ -2,9 +2,24 @@
 #include <filesystem>
 #include <string>
 #include <iostream>
+#include <sstream>
 
 #include "path_pattern.h"
 #include "util.h"
+
+void pathPatternSyntaxError(const std::string& str, int pos, const char* msg)
+{
+	std::stringstream out;
+	out << "\r\n" << str << "\r\n";
+	for (int i=0; i<pos; i++) out << ' ';
+	out << "^\r\n";
+	out << msg << "\r\n";
+	exitWithError(out.str());
+}
+void pathPatternSyntaxError(const std::string& str, int pos, const std::string& msg)
+{
+	pathPatternSyntaxError(str, pos, msg.c_str());
+}
 
 PathPattern_DirectoriesThatMatch::PathPattern_DirectoriesThatMatch(const std::string& _str, std::shared_ptr<PathPattern> _subPattern, bool _absolutePath):
 	str(_str), subPattern(_subPattern), absolutePath(_absolutePath)
@@ -82,10 +97,10 @@ void PathPattern::findFiles(const std::string& baseDirectory, std::function<void
 
 void PathPattern_DirectoriesThatMatch::findFiles(const std::string& baseDirectory, std::function<void(const std::string& path)> callback)
 {
-	//std::cout << "PathPattern_DirectoriesThatMatch(" << baseDirectory << ")\r\n";
+	if (DEBUGGING) std::cout << "PathPattern_DirectoriesThatMatch in " << baseDirectory << " on " << this->str << "\r\n";
 	if (this->absolutePath)
 	{
-		//std::cout << this->str << " is an absolute path!\r\n";
+		if (DEBUGGING) std::cout << "PathPattern_DirectoriesThatMatch::findFiles: " << this->str << " is an absolute path!\r\n";
 		this->subPattern->findFiles(this->str, callback);
 	}
 	else
@@ -101,15 +116,25 @@ void PathPattern_DirectoriesThatMatch::findFiles(const std::string& baseDirector
 			exitWithError(baseDirectory + " is not a directory!");
 			return;
 		}
-		
-		for (const auto& entry : std::filesystem::directory_iterator(baseDirectory))
-		{
-			if (!entry.is_directory()) continue;
 
-			const std::string& dir = entry.path().filename().string();
-			if (pathSegment_matches_patternSegment(dir, this->str))
+		if (this->str == "." || this->str == "..")
+		{
+			if (baseDirectory.back() != '/' && baseDirectory.back() != '\\') this->subPattern->findFiles(baseDirectory + "/" + this->str, callback);
+			else this->subPattern->findFiles(baseDirectory + this->str, callback);
+		}
+		else
+		{
+			for (const auto& entry : std::filesystem::directory_iterator(baseDirectory))
 			{
-				this->subPattern->findFiles(baseDirectory + "/" + dir, callback);
+				if (!entry.is_directory()) continue;
+
+				const std::string& dir = entry.path().filename().string();
+				if (DEBUGGING) std::cout << dir << "\r\n";
+				if (pathSegment_matches_patternSegment(dir, this->str))
+				{
+					if (baseDirectory.back() != '/' && baseDirectory.back() != '\\') this->subPattern->findFiles(baseDirectory + "/" + dir, callback);
+					else this->subPattern->findFiles(baseDirectory + dir, callback);
+				}
 			}
 		}
 	}
@@ -125,18 +150,16 @@ void PathPattern_Union::findFiles(const std::string& baseDirectory, std::functio
 
 void PathPattern_FilesThatMatch::findFiles(const std::string& baseDirectory, std::function<void(const std::string& path)> callback)
 {
-	std::cout << "PathPattern_FilesThatMatch::findFiles(" << baseDirectory << ") on " << this->str << "\r\n";
+	if (DEBUGGING) std::cout << "PathPattern_FilesThatMatch::findFiles(" << baseDirectory << ") on " << this->str << "\r\n";
 
 	if (!std::filesystem::exists(baseDirectory))
 	{
-		std::cerr << "Directory " << baseDirectory << " does not exist!\r\n";
-		return;
+		exitWithError("Directory " + baseDirectory + " does not exist!");
 	}
 
 	if (!std::filesystem::is_directory(baseDirectory))
 	{
-		std::cerr << baseDirectory << " is not a directory!\r\n";
-		return;
+		exitWithError(baseDirectory + " is not a directory!");
 	}
 	
 	for (const auto& entry : std::filesystem::directory_iterator(baseDirectory))
@@ -144,11 +167,12 @@ void PathPattern_FilesThatMatch::findFiles(const std::string& baseDirectory, std
 		if (!entry.is_regular_file()) continue;
 
 		const std::string& file = entry.path().filename().string();
-		std::cout << file << "\r\n";
+		if (DEBUGGING) std::cout << file << "\r\n";
 
 		if (pathSegment_matches_patternSegment(file, this->str))
 		{
-			callback(baseDirectory + "/" + file);
+			if (baseDirectory.back() != '/' && baseDirectory.back() != '\\') callback(baseDirectory + "/" + file);
+			else callback(baseDirectory + file);
 		}
 	}
 }
@@ -168,7 +192,7 @@ void PathPattern_AlsoCheckSubDirectoriesRecursively::findFiles(const std::string
 
 std::string readPatternPathSegment(const std::string& str, int& pos)
 {
-	if (DEBUGGING) std::cout << "readPatternPathSegment(" << str.substr(pos) << ")";
+	if (DEBUGGING) std::cout << "readPatternPathSegment(" << str.substr(pos) << ")\r\n";
 
 	const int startPos = pos;
 	while (pos < str.length() && str[pos] != '\\' && str[pos] != '/' && str[pos] != ',')
@@ -176,7 +200,7 @@ std::string readPatternPathSegment(const std::string& str, int& pos)
 		pos++;
 	}
 
-	if (pos == startPos) exitWithError("readPatternPathSegment failed");
+	if (pos == startPos) pathPatternSyntaxError(str, pos, "Expected file or directory name");
 
 	return str.substr(startPos, pos-startPos);
 }
@@ -198,18 +222,23 @@ std::shared_ptr<PathPattern> _parsePathPattern(const std::string& str, int& pos,
 		}
 		else
 		{
+			int startPos = pos;
 			absolutePathPrefix = readPatternPathSegment(str, pos);
 			if (DEBUGGING) std::cout << "absolutePathPrefix=" << absolutePathPrefix << "\r\n";
+			char slashAfterAbsolutePathPrefix;
 			if (str[pos] == '/' || str[pos] == '\\')
 			{
+				slashAfterAbsolutePathPrefix = str[pos];
 				pos++;
 			}
 			else
 			{
-				exitWithError(std::string("Expected / or \\, found '") + str[pos] + "'");
+				pathPatternSyntaxError(str, pos, "Expected path delimiter: / or \\");
 			}
 
-			if (std::filesystem::path(absolutePathPrefix + "/").is_absolute())
+			absolutePathPrefix += slashAfterAbsolutePathPrefix;
+
+			if (std::filesystem::path(absolutePathPrefix).is_absolute())
 			{
 				if (DEBUGGING) std::cout << "'" << absolutePathPrefix << "' is absolute!\r\n";
 				absolutePath = true;
@@ -217,7 +246,7 @@ std::shared_ptr<PathPattern> _parsePathPattern(const std::string& str, int& pos,
 			else
 			{
 				if (DEBUGGING) std::cout << "'" << absolutePathPrefix << "' is not absolute!\r\n";
-				pos--;
+				pos = startPos;
 			}
 		}
 	}
@@ -248,7 +277,7 @@ std::shared_ptr<PathPattern> _parsePathPattern(const std::string& str, int& pos,
 		}
 		else
 		{
-			exitWithError("wtffff 923848934 " + str + " " + std::to_string(pos));
+			pathPatternSyntaxError(str, pos, "Expected path delimiter: / or \\");
 			return nullptr;
 		}
 	}
@@ -264,7 +293,7 @@ std::shared_ptr<PathPattern> parsePathPattern(const std::string& pattern)
 	{
 		patterns.push_back(_parsePathPattern(pattern, pos, true));
 		if (pos >= pattern.length()) break;
-		if (pattern[pos] != ',') exitWithError("Unexpected char in pattern " + pattern + " " + pattern[pos]);
+		if (pattern[pos] != ',') pathPatternSyntaxError(pattern, pos, std::string("Unexpected character in path pattern: '") + pattern[pos] + "'");
 		pos++;
 	}
 
