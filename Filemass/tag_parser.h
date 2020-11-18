4cbaf3caff47c4af3e778c48306fac4ea1b2dd8a
Jesse Busman <jesse@jesbus.com>
2020-11-18 12:17:01 +0100

    Delete vs stuff

--- Filemass/tag_parser.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <memory>
-#include <string>
-#include <vector>
-
-#include "tag.h"
-
-std::vector<std::shared_ptr<Tag>> parseTag(std::string str);
--- Filemass/tag_query_parser.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include "tag_query.h"
-
-std::shared_ptr<TagQuery> parseTagQuery(const std::string& str);
